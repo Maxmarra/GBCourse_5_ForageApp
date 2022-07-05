ffceb49b92bd43dbebd0620404c3feb6d4d2c7af
Maxmarra <5919669@gmail.com>
2022-07-05 10:44:02 +0300

    clean starter

---
+++ app/src/main/java/com/example/forageapp/BaseApplication.kt
@@ -0,0 +1,13 @@
+package com.example.forageapp
+
+import android.app.Application
+import com.example.forageapp.data.ForageDatabase
+
+/**
+ * An application class that inherits from [Application], allows for the creation of a singleton
+ * instance of the [ForageDatabase]
+ */
+class BaseApplication : Application() {
+
+    // TODO: provide a ForageDatabase value by lazy here
+}
